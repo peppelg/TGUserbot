7f92fb84a79f36f10f4e44f1e038460f663cd801
Peppe Lg <peppelg@bmtrad.it>
2017-06-09 20:14:37 +0200

    🌚

--- bot.php
+++ bot.php
@@ -4,6 +4,7 @@ if($msg == "/start") {
   sm($chatID, "Ciao! Questo è un messaggio con risposta.", 1); //con 1 il bot risponderà al messaggio
   sm($chatID, "Ciao! Questo è un messaggio senza risposta.");
 }
+
 if($msg == "/info") {
   sm($chatID, "ChatID: $chatID \n UserID: $userID \n Tipo: $type");
 }
