a81d2cd2366251e96ddd79fe2dc84907b620ecf2
Giuseppe <peppelg@bmtrad.it>
2017-12-31 14:41:30 +0100

    :fire: TGUserbot V2. :tada: Buon 2018!

---
+++ settings.php
@@ -0,0 +1,10 @@
+<?php
+$settings = [
+  'language' => 'it',
+  'session' => 'session.madeline',
+  'send_errors' => true,
+  'readmsg' => true,
+  'always_online' => false,
+  'old_chatinfo' => false,
+  'auto_reboot' => true
+];
