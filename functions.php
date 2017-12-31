cb80b80051252bbe36457d0d9c1730029e180dce
Giuseppe <peppelg@bmtrad.it>
2017-12-31 14:41:58 +0100

    :fire: TGUserbot V2. :tada: Buon 2018!

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+function sm($chatID, $text, $reply = 0, $parsemode = 'HTML') {
+  global $update;
+  global $MadelineProto;
+  if (isset($chatID) and isset($text) and $reply == 0) $MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => $text, 'parse_mode' => $parsemode]);
+  if (isset($chatID) and isset($text) and $reply == 1) $MadelineProto->messages->sendMessage(['peer' => $chatID, 'message' => $text, 'reply_to_msg_id' => $update['update']['message']['id'], 'parse_mode' => $parsemode]);
+}
