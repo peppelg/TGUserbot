4ac15f2223a965a3619b3ab8ebe02bc84887d1a3
Giuseppe <peppelg@bmtrad.it>
2017-12-31 17:09:25 +0100

    :fire: TGUserbot V2. :tada: Buon 2018!

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 if (isset($update['update']['message']['out']) and $update['update']['message']['out'] == true) return 0;
 if (!isset($msg)) return 0;
-if ($msg == '/info' and $userID == 59625577) {
+if ($msg == '/info') {
   sm($chatID, "<b>Info chat:</b>\nID: $chatID\nTitolo: $title\nUsername chat: @$chatusername\nTipo: $type\n\n<b>Informazioni utente:</b>\nID: $userID\nNome: $name\nUsername: @$username", 1);
 }
