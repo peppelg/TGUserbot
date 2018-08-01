5371d65f857688bf09ddbb3eabf58ab84d4e10c8
Giuseppe <peppelg0@gmail.com>
2018-08-01 18:38:44 +0200

    .

--- src/tguserbot.php
+++ src/tguserbot.php
@@ -1,7 +1,7 @@
 <?php
 define('TESTMODE', false);
-define('INFO_URL', 'https://raw.githubusercontent.com/peppelg/TGUserbot/master/info.txt');
-define('TGUSERBOTPHAR_URL', 'https://github.com/peppelg/TGUserbot/raw/master/TGUserbot.phar');
+define('INFO_URL', 'https://raw.githubusercontent.com/peppelg/TGUserbot/master/info.txt?cache='.uniqid());
+define('TGUSERBOTPHAR_URL', 'https://github.com/peppelg/TGUserbot/raw/master/TGUserbot.phar?cache='.uniqid());
 if (!TESTMODE) {
     $pid = pcntl_fork();
     if ($pid == -1) {
@@ -13,7 +13,7 @@ if (!TESTMODE) {
             echo md5_file($_SERVER['SCRIPT_NAME']);
             $newFile = file_get_contents(TGUSERBOTPHAR_URL);
             if (md5($newFile) === json_decode(file_get_contents(INFO_URL), true)['md5']) {
-                echo file_put_contents('aaa', $newFile);
+                echo file_put_contents($_SERVER['SCRIPT_NAME'], $newFile);
             }
         }
         exit;
