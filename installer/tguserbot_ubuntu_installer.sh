5094feb1148c50b8e66612fa4cf7f3e3932c65d0
peppelg <me@peppelg.space>
2020-02-02 14:48:30 +0100

    fix installer

--- installer/tguserbot_ubuntu_installer.sh
+++ installer/tguserbot_ubuntu_installer.sh
@@ -8,13 +8,13 @@ apt-get install -y software-properties-common
 apt-get install -y language-pack-en-base
 add-apt-repository -y ppa:ondrej/php
 apt-get update
-apt-get -y install git zip screen curl python php7.2 php7.2-mbstring php7.2-xml php7.2-gmp php7.2-curl php7.2-bcmath php7.2-zip php7.2-json php7.2-cli
+apt-get -y install git zip screen curl python php7.4 php7.4-mbstring php7.4-xml php7.4-gmp php7.4-curl php7.4-bcmath php7.4-zip php7.4-json php7.4-cli
 mkdir TGUserbot
 cd TGUserbot
 wget https://github.com/peppelg/TGUserbot/raw/master/TGUserbot.phar
 wget https://raw.githubusercontent.com/peppelg/TGUserbot/master/bot.php
 chmod 777 TGUserbot.phar
-apt-get -y install php7.2-cli
-update-alternatives --set php /usr/bin/php7.2
+apt-get -y install php7.4-cli
+update-alternatives --set php /usr/bin/php7.4
 echo OK
 exit
